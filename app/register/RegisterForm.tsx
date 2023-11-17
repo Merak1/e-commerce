d6c7ac21b243dc0be479b18b2d2aa202f8ce6d20
root <root@DESKTOP-DH9FK14.localdomain>
2023-11-17 17:51:25 -0600

    added api endppoint to db to register user

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,11 @@ import { AiOutlineAccountBook, AiOutlineGoogle } from "react-icons/ai";
 import Heading from "../components/Heading";
 import Button from "../components/Button";
 import Input from "../components/inputs/input";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { Router } from "next/router";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,9 +27,36 @@ const RegisterForm = () => {
     },
   });
 
+  const router = useRouter();
+
   const onSubmit: SubmitHandler<FieldValues> = async (data: FieldValues) => {
     setIsLoading(true);
-    console.log(data);
+    axios.post("/api/register", data).then(() => {
+      toast.success("Account created successfully");
+
+      signIn("credentials", {
+        email: data.email,
+        password: data.password,
+        redirectUrl: false,
+      })
+        .then((callback) => {
+          if (callback?.ok) {
+            router.push("/cart");
+            router.refresh();
+            toast.success("Logged in");
+          }
+          if (callback?.error) {
+            toast.error(callback.error);
+          }
+        })
+        .catch((error) => {
+          toast.error(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    });
+
     setIsLoading(false);
   };
 
