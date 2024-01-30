35be6cea981935b6c2906f146e57358d1fb6f35e
root <root@DESKTOP-DH9FK14.localdomain>
2024-01-30 16:34:25 -0600

    added product categories and colors

---
+++ utils/categories.tsx
@@ -0,0 +1,114 @@
+import {
+  TbNumber10Small,
+  TbNumber11Small,
+  TbNumber12Small,
+  TbNumber13Small,
+  TbNumber14Small,
+  TbNumber15Small,
+  TbNumber1Small,
+  TbNumber2Small,
+  TbNumber3Small,
+  TbNumber4Small,
+  TbNumber5Small,
+  TbNumber6Small,
+  TbNumber7Small,
+  TbNumber8Small,
+  TbNumber9Small,
+} from "react-icons/tb";
+
+export const categories = {
+  CÁMARA_METALICA_COLORES: "cámara metalica colores",
+  TRANSPARENTE_ESQUINA_COLOR: "transparente esquina color",
+  NEGRAS_CON_TEXTURA: "negras con textura",
+  MAGSAFE: "magsafe",
+  GRADOS_360: "360 grados",
+  EJECUTIVAS_COLORES: "ejecutivas colores",
+  PROTECTOR_CÁ_MARA_PASTEL: "protector cámara pastel",
+  CORREA: "correa",
+  MATE: "mate",
+  NEON: "neón",
+  PROTECTORES_DE_CAMARA: "protectores de camara",
+  ACRIGEL: "acrigel",
+  SUBLIMAR: "sublimar",
+  AIRPODS: "airpods",
+  SUBLIMAR_TRANSPARENTE: "sublimar transparente",
+};
+
+export const categoryButtons = [
+  {
+    label: "cámara metalica colores",
+    icon: TbNumber1Small,
+  },
+  {
+    label: "transparente esquina color",
+    icon: TbNumber2Small,
+  },
+  {
+    label: "negras con textura",
+    icon: TbNumber3Small,
+  },
+  {
+    label: "magsafe",
+    icon: TbNumber4Small,
+  },
+  {
+    label: "360 grados",
+    icon: TbNumber5Small,
+  },
+  {
+    label: "ejecutivas colores",
+    icon: TbNumber6Small,
+  },
+  {
+    label: "protector cámara pastel",
+    icon: TbNumber7Small,
+  },
+  {
+    label: "correa",
+    icon: TbNumber8Small,
+  },
+  {
+    label: "mate",
+    icon: TbNumber9Small,
+  },
+  {
+    label: "neón",
+    icon: TbNumber10Small,
+  },
+  {
+    label: "protectores de cámara",
+    icon: TbNumber11Small,
+  },
+  {
+    label: "acrigel",
+    icon: TbNumber12Small,
+  },
+  {
+    label: "sublimar",
+    icon: TbNumber13Small,
+  },
+  {
+    label: "airpods",
+    icon: TbNumber14Small,
+  },
+  {
+    label: "sublimar transparente",
+    icon: TbNumber15Small,
+  },
+];
+
+// cámara metalica colores
+//  transparente esquina color
+//  negras con textura
+//  magsafe
+//  360 grados
+//  ejecutivas colores
+//  protector cámara pastel
+//  correa
+//  mate
+//  neón
+//  protectores de camara
+//  acrigel
+//  sublimar
+//  airpods
+//  sublimar transparente
---
+++ utils/productsColors.tsx
@@ -0,0 +1,22 @@
+export const productColors = [
+  { color: "Azul", colorCode: "#07177c", image: null },
+  { color: "Rojo", colorCode: "#dd3333", image: null },
+  { color: "azul grisaceo", colorCode: "#729abf", image: null },
+  { color: "lila", colorCode: "#be9de0", image: null },
+  { color: "menta", colorCode: "#75f4ac", image: null },
+  { color: "negro", colorCode: "#000000", image: null },
+  { color: "rosa", colorCode: "#f490da", image: null },
+  { color: "transparente", colorCode: "#ffffff", image: null },
+  { color: "verde militar", colorCode: "#667c00", image: null },
+  { color: "verde obscuro", colorCode: "#005419", image: null },
+];
+// azul #07177c
+// azul grisaceo #729abf
+// lila #be9de0
+// menta #75f4ac
+// negro #000000
+// rojo #dd3333
+// rosa #f490da
+// transparente #ffffff
+// verde mitlitar #667c00
+// verde obscuro #005419
