ba88b85c723165e487fa154ac3700accdfca329d
root <root@DESKTOP-DH9FK14.localdomain>
2024-02-16 22:36:55 -0600

    deleted comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default async function RootLayout({
         <CartProvider>
           <div className="flex flex-col min-h-screen ">
             <NavBar />
-            {/* <NavBar /> */}
             <main className="flex-grow ">{children}</main>
             <Footer />
           </div>
