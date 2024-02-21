065833d9e686ddf027156b3302ad52a090ed3324
root <root@DESKTOP-DH9FK14.localdomain>
2024-02-21 17:09:40 -0600

    added stats summary

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,28 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import { redirect } from "next/navigation";
+import Summary from "./Summary";
+import getProducts from "@/actions/getProducts";
+import getOrders from "@/actions/getOrders";
+import getUsers from "@/actions/getUsers";
+import Container from "../components/Container";
 
 const Admin = async () => {
   const currentUser = await getCurrentUser();
+
+  const products = await getProducts({ category: null });
+  const orders = await getOrders();
+  const users = await getUsers();
+
   if (!currentUser || currentUser.role !== "ADMIN") {
     redirect("/");
   }
-  return <div className="pt-8">admin page</div>;
+  return (
+    <div className="pt-8">
+      <Container>
+        <Summary products={products} orders={orders} users={users} />
+      </Container>
+    </div>
+  );
 };
 
 export default Admin;
