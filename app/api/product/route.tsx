ff55b4ad46bc12bd52dfef931614cef1d4f8a01c
root <root@DESKTOP-DH9FK14.localdomain>
2024-02-22 15:08:30 -0600

    modified api user verification

--- app/api/product/route.tsx
+++ app/api/product/route.tsx
@@ -4,43 +4,16 @@ import { getCurrentUser } from "@/actions/getCurrentUser";
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
-
-  // if (currentUser?.role !== "ADMIN") {
-  //   // return NextResponse.error();
-  //   // toast.error("NO ADMIN");
-  //   console.log("NO ADMIN");
-  // }
-  // if (!currentUser) {
-  //   // toast.error("NO CURRENT USER");
-  //   console.log("NO CURRENT USER");
-  //   // return NextResponse.error();
-  // }
-
-  // // if (currentUser?.role !== "ADMIN") {
-  // //   return NextResponse.error();
-  // // }
-
-  const body = await request.json();
-  const {
-    id,
-    name,
-    description,
-    price,
-    brand,
-    category,
-    inStock,
-    images,
-    sku,
-    model,
-    sales,
-  } = body;
-
-  const product = await prisma.product.create({
-    data: {
+  if (currentUser) {
+    if (currentUser?.role !== "ADMIN") {
+      return NextResponse.error();
+    }
+    const body = await request.json();
+    const {
       id,
       name,
       description,
-      price: parseFloat(price),
+      price,
       brand,
       category,
       inStock,
@@ -48,17 +21,33 @@ export async function POST(request: Request) {
       sku,
       model,
       sales,
-    },
-  });
+    } = body;
 
-  return NextResponse.json(product);
+    const product = await prisma.product.create({
+      data: {
+        id,
+        name,
+        description,
+        price: parseFloat(price),
+        brand,
+        category,
+        inStock,
+        images,
+        sku,
+        model,
+        sales,
+      },
+    });
+
+    return NextResponse.json(product);
+  }
 }
 
 export async function PUT(request: Request) {
   const currentUser = await getCurrentUser();
   if (!currentUser) return NextResponse.error();
 
-  if (currentUser.role === "ADMIN") {
+  if (currentUser.role !== "ADMIN") {
     return NextResponse.error();
   }
   const body = await request.json();
