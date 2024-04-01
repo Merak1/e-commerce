4ae11c0d531707ef90206d0c783ee4ea74ecf1e0
root <root@DESKTOP-DH9FK14.localdomain>
2024-04-01 14:33:48 -0600

    modified custom input component to have sm variant

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -11,6 +11,7 @@ interface InputProps {
   required?: boolean;
   register: UseFormRegister<FieldValues>;
   errors: FieldErrors;
+  sm?: boolean;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -21,6 +22,7 @@ const Input: React.FC<InputProps> = ({
   required,
   register,
   errors,
+  sm,
 }) => {
   return (
     <div className="w-full relative mt-3">
@@ -32,7 +34,9 @@ const Input: React.FC<InputProps> = ({
         required={required}
         placeholder=""
         {...register(id, { required })}
-        className={`peer w-full p-4 pt-6 outline-none bg-white
+        className={`peer w-full 
+        ${sm === true ? "p-2 pt-3" : "p-4 pt-6"}
+        outline-none bg-white
         font-light border-2 rounded-md transition disabled:opacity-70 
         disabled:cursor-not-allowed 
         ${errors[id] ? "border-rose-400" : "border-slate-300"}
@@ -42,10 +46,16 @@ const Input: React.FC<InputProps> = ({
       <label
         className={`absolute cursor-text text-md
         duration-150 transform 
-        -translate-y-3 top-5 z-10 origin-[0]
+         ${
+           sm === true
+             ? "-translate-y-3 top-3 z-5 "
+             : "  -translate-y-3 top-4 z-10"
+         }
+        origin-[0]
         left-4 peer-placeholder-shown:scale-100
         peer-placeholder-shown:translate-y-0 
-        peer-focus:scale-75 peer-focus:-translate-y-4 capitalize
+        peer-focus:scale-75 peer-focus:-translate-y-4
+        capitalize
          ${errors[id] ? "text-rose-500" : "text-slate-400"}
        `}
         htmlFor={id}
